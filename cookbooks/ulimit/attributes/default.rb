884205477e9fef1fc9d1a5afc0ef1690899c74a0
Miner Operations <minerops@ip-10-0-3-215.ec2.internal>
2015-10-15 20:53:38 +0000

    ulimit

--- cookbooks/ulimit/attributes/default.rb
+++ cookbooks/ulimit/attributes/default.rb
@@ -1,3 +1,35 @@
+
+
 default['ulimit']['pam_su_template_cookbook'] = nil
-default['ulimit']['users'] = Mash.new
+default['ulimit']['users'] = {
+  :minerops => {
+  :filehandle_limit => 8192, # optional
+  :filehandle_soft_limit => 8192, # optional; not used if filehandle_limit is set)
+  :filehandle_hard_limit => 8192, # optional; not used if filehandle_limit is set)
+  :process_limit => 61504, # optional
+  :process_soft_limit => 61504, # optional; not used if process_limit is set)
+  :process_hard_limit => 61504, # optional; not used if process_limit is set)
+  :memory_limit => 1024,# optional
+  :core_limit => 2048, # optional
+  :core_soft_limit => 1024, # optional
+  :core_hard_limit => 'unlimited', # optional
+  :stack_soft_limit => 2048, # optional
+  :stack_hard_limit => 2048 # optional
+  },
+  :ubuntu => {
+  :filehandle_limit => 8192, # optional
+  :filehandle_soft_limit => 8192, # optional; not used if filehandle_limit is set)
+  :filehandle_hard_limit => 8192, # optional; not used if filehandle_limit is set)
+  :process_limit => 61504, # optional
+  :process_soft_limit => 61504, # optional; not used if process_limit is set)
+  :process_hard_limit => 61504, # optional; not used if process_limit is set)
+  :memory_limit => 1024,# optional
+  :core_limit => 2048, # optional
+  :core_soft_limit => 1024, # optional
+  :core_hard_limit => 'unlimited', # optional
+  :stack_soft_limit => 2048, # optional
+  :stack_hard_limit => 2048 # optional
+  }
+}
 default['ulimit']['security_limits_directory'] = '/etc/security/limits.d'
+
