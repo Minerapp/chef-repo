33a3f87d9ef8f79a3618d4cab06e084fc8ba399f
Michael Hernandez <michael.hernandez1988@gmail.com>
2015-10-17 11:56:23 -0400

    sudoers

--- cookbooks/miner_users/recipes/dev.rb
+++ cookbooks/miner_users/recipes/dev.rb
@@ -1,6 +1,6 @@
-node.default['authorization']['sudo']['passwordless'] = true
-node.default['authorization']['sudo']['groups'] = ["minerops", "sysadmin", "wheel", "admin"]
-node.default['authorization']['sudo']['include_sudoers_d'] = true
+# node.default['authorization']['sudo']['passwordless'] = true
+# node.default['authorization']['sudo']['groups'] = ["minerops", "sysadmin", "wheel", "admin"]
+# node.default['authorization']['sudo']['include_sudoers_d'] = true
 
  users_manage "minerdevservicedeployers" do
    group_id 3000
