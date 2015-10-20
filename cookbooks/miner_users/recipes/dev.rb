fa28e1116a2add63384ef1fc4034dea4ed48be42
Michael Hernandez <michael.hernandez1988@gmail.com>
2015-10-20 13:47:35 -0400

    fixsudo

--- cookbooks/miner_users/recipes/dev.rb
+++ cookbooks/miner_users/recipes/dev.rb
@@ -31,7 +31,7 @@ end
 sudo 'minerdevservicedeployers' do
   group     "%minerdevservicedeployers"    # or a username
   nopasswd true
-  # runas     "minerdevservicedeploy:minerdevservicedeployers"
+  runas     "root:minerdevservicedeployers"
   commands ['/usr/local/bin/pm2', '/bin/echo']
   env_keep_add ['PM2_HOME', 'NODE_ENV', 'DEBUG']
 end
